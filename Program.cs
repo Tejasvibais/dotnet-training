2802796ada2a5ea343dbc3fd7ec10260f73e1739
Tejasvi Bais <tejasvibais29@gmail.com>
2022-05-26 12:25:30 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Max1, Max2;
+            int[] arr = { 12, 34, 54, 59,45,42 };
+            Console.WriteLine("Array Elements are : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Max1 = Max2 = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > Max1)
+                {
+                    Max2 = Max1;
+                    Max1 = arr[i];
+                }
+                else if (arr[i] > Max2)
+                {
+                    Max2 = arr[i];
+                }
+                else if (arr[i] < Max2 && Max1 == Max2)
+                {
+                    Max2 = arr[i];
+                }
+            }
+            Console.WriteLine("\nFisrt Max Number : " + Max1);
+            Console.WriteLine("Second Max Number : " + Max2);
+            Console.ReadLine();
+        }
+    }
+}
+    
+
